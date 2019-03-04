e39794c8e96222b22626c1ff313263aa27ffc123
sihai <464990030@qq.com>
2019-03-04 21:27:06 +0800

    56

--- algorithm/subArray.js
+++ algorithm/subArray.js
@@ -1,4 +1,24 @@
 /**
+ * 53.Maximum Subarray
+ * Input: [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var pre = 0
+  var max = -Number.MAX_VALUE
+
+  for(var i = 0; i < nums.length; i++) {
+    pre = Math.max(pre + nums[i], nums[i])
+    max = Math.max(pre, max)
+  }
+
+  return sum
+};
+
+/**
  * 209.Minimum Size Subarray Sum
  * Input: s = 7, nums = [2,3,1,2,4,3]
  * Output: 2
