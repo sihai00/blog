747382fe5ea38b197ce58120d9d3d55eb31cbf67
sihai <464990030@qq.com>
2019-03-06 00:03:24 +0800

    90 128

--- algorithm/subArray.js
+++ algorithm/subArray.js
@@ -36,18 +36,96 @@ var maxSubArray = function(nums) {
  * @return {number[][]}
  */
 var subsets = function(nums) {
-  const sets = [[]];
+  var arr = [[]];
 	
-	for(let number of nums) {
-		let setLength = sets.length;
-		for(let i = 0; i < setLength; i++) {
-			const setToAdd = sets[i].slice();
-			setToAdd.push(number);
-			sets.push(setToAdd);
-		}
-	}
+  for(var i = 0; i < nums.length; i++) {
+    var len = arr.length
+    for(var j = 0; j < len; j++) {
+      var add = arr[j].slice()
+      add.push(nums[i])
+      arr.push(add)
+    }
+  }
 	
-	return sets;
+	return arr;
+};
+
+/**
+ * 90.Subsets II
+ * Input: nums = [1,2,2]
+ * Output:
+ * [
+ *   [2],
+ *   [1],
+ *   [1,2,2],
+ *   [2,2],
+ *   [1,2],
+ *   []
+ * ]
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+  var nums = nums.sort((a, b) => a - b)
+  var arr = [[]]
+
+  for(var i = 0; i < nums.length; i++) {
+    var startIndex = i > 0 && nums[i] === nums[i - 1] ? len : 0
+    var len = arr.length
+    for(var j = startIndex; j < len; j++) {
+      var add = arr[j].slice()
+      add.push(nums[i])
+      arr.push(add)
+    }
+  }
+
+  return arr
+};
+
+/**
+ * 128.Longest Consecutive Sequence
+ * Input: [100, 4, 200, 1, 3, 2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+  var nums = nums.sort((a, b) => a - b)
+  var set = [...new Set(nums)]
+  var l = 0
+  var max = set.length > 0 ? 1 : 0
+
+  for(var i = 1; i < set.length; i++) {
+    if (set[i] - set[i - 1] === 1){
+      max = Math.max(max, i - l + 1)
+    } else {
+      l = i
+    }
+  }
+
+  return max
+};
+
+var longestConsecutive = function(nums) {
+  if (nums.length === 0) return 0
+  nums.sort((a, b) => a - b)
+
+  var result = 1
+  var max = 1
+
+  for(var i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      if (nums[i] - nums[i - 1] === 1) {
+        result += 1
+      } else {
+        max = Math.max(max, result)
+        result = 1
+      }
+    }
+  }
+
+  return Math.max(max, result)
 };
 
 /**
