2690453066f326e8d9f54976636a1faa93c021ee
sihai <464990030@qq.com>
2019-04-04 17:31:20 +0800

    92

--- algorithm/92reverseBetween.js
+++ algorithm/92reverseBetween.js
@@ -1,4 +1,9 @@
 /**
+ * 92.Reverse Linked List II
+ * Example:
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4
+ * Output: 1->4->3->2->5->NULL
+ *
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
@@ -11,6 +16,23 @@
  * @param {number} n
  * @return {ListNode}
  */
-var reverseBetween = function(head, m, n) {
+function reverseBetween(head, m, n) {
+  const before = new ListNode();
+  let prev = before;
+  prev.next = head;
 
-};
+  while (--m) {
+    prev = prev.next;
+    --n;
+  }
+
+  let curr = prev.next;
+  while (--n) {
+    let next = curr.next;
+    curr.next = next.next;
+    next.next = prev.next;
+    prev.next = next;
+  }
+
+  return before.next;
+}
