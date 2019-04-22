a8ca35a6722a2ee609cfe151237d04aa531fe957
sihai <464990030@qq.com>
2019-04-22 09:17:32 +0800

    234

--- algorithm/234isPalindrome.js
+++ algorithm/234isPalindrome.js
@@ -31,3 +31,47 @@ var isPalindrome = function(head) {
 
   return first === second
 };
+
+var isPalindrome = function(head) {
+  function findMid(head){
+    var slow = head
+    var fast = head.next
+
+    while(fast && fast.next) {
+      slow = slow.next
+      fast = fast.next.next
+    }
+
+    return slow
+  }
+  function reverse(head){
+    var pre = null
+
+    while(head){
+      var next = head.next
+      head.next = pre
+      pre = head
+      head = next
+    }
+
+    return pre
+  }
+  function compare(head1, head2){
+    while(head1 && head2){
+      if (head1.val !== head2.val) return false
+
+      head1 = head1.next
+      head2 = head2.next
+    }
+
+    return true
+  }
+
+  if (!head || !head.next) return true
+
+  var mid = findMid(head)
+  var reverHead = reverse(mid.next)
+
+  mid.next = null
+  return compare(head, reverHead)
+};
