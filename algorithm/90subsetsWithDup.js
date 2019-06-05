ae104c0c516dfd7606f24c06d5173cdc2ec48444
sihai <464990030@qq.com>
2019-06-05 23:08:03 +0800

    90

--- algorithm/90subsetsWithDup.js
+++ algorithm/90subsetsWithDup.js
@@ -1,7 +1,36 @@
 /**
+ * 90. Subsets II
+ * Example:
+ * Input: [1,2,2]
+ * Output:
+ * [
+ *   [2],
+ *   [1],
+ *   [1,2,2],
+ *   [2,2],
+ *   [1,2],
+ *   []
+ * ]
+ *
  * @param {number[]} nums
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
+  nums = nums.sort((a, b) => a - b)
+  var res = []
 
+  function helper(nums, start, arr){
+    res.push(arr)
+
+    var last = ''
+    for (var i = start; i < nums.length; i++) {
+      if (nums[i] === last) continue
+      last = nums[i]
+      helper(nums, i + 1, arr.concat([nums[i]]))
+    }
+  }
+
+  helper(nums, 0, [])
+
+  return res
 };
