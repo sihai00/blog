8bd7c4b0ca619186e08982655b14cb6f894ed2b0
sihai <464990030@qq.com>
2019-06-08 00:32:09 +0800

    79

--- algorithm/79exist.js
+++ algorithm/79exist.js
@@ -16,5 +16,46 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-  
+  var d = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+  var m, n
+  var visited = Array(board.length).fill([])
+
+  function inArea(x, y) {
+    return x >= 0 && x < m && y >= 0 && y < n
+  }
+
+  function searchWord(board, word, index, startx, starty){
+    // 最后一个字母
+    if (word.length - 1 === index) {
+      return board[startx][starty] === word[index]
+    }
+
+    // 四个方向寻找
+    if (board[startx][starty] === word[index]) {
+      visited[startx][starty] = true
+
+      for (var i = 0; i < 4; i++) {
+        var newx = startx + d[i][0]
+        var newy = starty + d[i][1]
+
+        if (inArea(newx, newy) && !visited[startx][starty]) {
+          return searchWord(board, word, index + 1, newx, newy)
+        }
+      }
+
+      visited[startx][starty] = false
+    }
+
+    return false
+  }
+
+  m = board.length
+  n = board[0].length
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      return searchWord(board, word, 0, i, j)
+    }
+  }
+
+  return false
 };
