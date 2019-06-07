0361e54ad6d7b3a94c779c55f29ceefd0bf2eb36
sihai <464990030@qq.com>
2019-06-07 23:23:59 +0800

    79

---
+++ algorithm/79exist.js
@@ -0,0 +1,20 @@
+/**
+ * 79. Word Search
+ * Example:
+ * board =
+ * [
+ *   ['A','B','C','E'],
+ *   ['S','F','C','S'],
+ *   ['A','D','E','E']
+ * ]
+ * Given word = "ABCCED", return true.
+ * Given word = "SEE", return true.
+ * Given word = "ABCB", return false.
+ *
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+  
+};
