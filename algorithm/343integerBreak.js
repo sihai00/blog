8b01ce23f7563590a507ca4b363caaab36543c42
sihai <464990030@qq.com>
2019-06-24 11:27:30 +0800

    343

--- algorithm/343integerBreak.js
+++ algorithm/343integerBreak.js
@@ -14,5 +14,33 @@
  * @return {number}
  */
 var integerBreak = function(n) {
-  
+  var memo = []
+
+  function breakInteger(n) {
+    if (n === 1) return 1
+    if (memo[n]) return memo[n]
+    var res = -1
+
+    for (var i = 1; i <= n - 1; i++) {
+      res = Math.max.apply(null, [res, i * (n - i), i * breakInteger(n - i)])
+    }
+
+    memo[n] = res
+    return res
+  }
+
+  return breakInteger(n)
+};
+
+var integerBreak = function(n) {
+  var memo = Array(n + 1).fill(-1)
+  memo[1] = 1
+
+  for (var i = 2; i <= n; i++) {
+    for (var j = 1; j <= i - 1; j++) {
+      memo[i] = Math.max.apply(null, [memo[i], j * (i - j), j * memo[i - j]])
+    }
+  } 
+
+  return memo[n]
 };
