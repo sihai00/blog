3ea6a680c3f6c376d4d617f1381a72572748d7db
sihai <464990030@qq.com>
2019-07-11 23:30:26 +0800

    322

---
+++ algorithm/322coinChange.js
@@ -0,0 +1,18 @@
+/**
+ * 322. Coin Change
+ * Example 1:
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ *
+ * Example 2:
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ *
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+
+};
