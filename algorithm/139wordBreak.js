e88088df4a1c0c0fbc9669bbc0fe2521bde66b65
sihai <464990030@qq.com>
2019-07-21 09:21:22 +0800

    139

--- algorithm/139wordBreak.js
+++ algorithm/139wordBreak.js
@@ -41,3 +41,17 @@ var wordBreak = function(s, wordDict) {
 
   return dp(s, wordDict)
 };
+
+var wordBreak = function(s, wordDict) {
+  let dp = new Array(s.length+1).fill(false);
+  dp[0] = true;
+  for(let i = 1; i <= s.length; i++) {
+    for(let j = 0; j < i; j++) {
+      if(dp[j] && wordDict.indexOf(s.substring(j,i)) !== -1){
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[dp.length-1]
+};
